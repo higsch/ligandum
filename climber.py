d03996fa162b9af318925bc0e800fa5a6c35f1e2
Matthias Stahl <matthias.stahl87@web.de>
2017-11-09 23:40:42 +0100

    added climber class (peak picking)

--- climber.py
+++ climber.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+default_params = {
+        'INTENSITY_PERCENTILE': 0.5,
+        'RT_OFFSET_TO_MS2'    : 1
+    }
+
+class Climber:
+    
+    def __init__(
+            self,
+            params = None
+                ):
+        
+        if params is None:
+            self.params = default_params
+        else:
+            self.params = params
+        
+        pass
+    return
